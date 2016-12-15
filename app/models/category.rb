a41202c6a92551ab63872b8d26849a67a8d44436
BriOD <briodonoghue@gmail.com>
2016-12-15 15:19:00 -0500

    added item and category_item models. linked up associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :menu
+  has_many :category_items
+  has_many :items, :through => :category_items
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161215191934) do
+ActiveRecord::Schema.define(version: 20161215201510) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.integer  "menu_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "category_items", force: :cascade do |t|
+    t.integer  "item_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.decimal  "price"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "menus", force: :cascade do |t|
