c5c6148f3eb5e1e1e8e89111b5a2cb2e59b3cfae
BriOD <briodonoghue@gmail.com>
2016-12-15 16:42:06 -0500

    added actions to categories controller, and menus controller. having issue with CoffeScript

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,23 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @category = Category.find_by_id(params[:id])
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,2 +1,23 @@
 class MenusController < ApplicationController
+  def index
+    @menus = Menu.all
+  end
+
+  def show
+    @menu = Menu.find_by_id(params[:id])
+  end
+
+  def new
+    @menu = Menu.new
+  end
+
+  def create
+    @menu = Menu.new(menu_params)
+  end
+
+  private
+
+  def menu_params
+    params.require(:menu).permit(:name)
+  end
 end
