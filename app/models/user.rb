0f17b91081cbd1c10135a4d6b0980c18422f754a
BriOD <briodonoghue@gmail.com>
2016-12-21 19:39:25 -0500

    started impementing devise role, added role column to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,15 @@ class User < ApplicationRecord
      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
        user.email = auth.info.email
        user.password = Devise.friendly_token[0,20]
-     end      
+     end
    end
+
+   enum role: [:normal, :admin]
+
+   after_initialize :default_to_role
+
+   def default_to_role
+     self.role ||= :normal
+   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161220234930) do
+ActiveRecord::Schema.define(version: 20161222003727) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 20161220234930) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
+    t.integer  "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["provider"], name: "index_users_on_provider"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
